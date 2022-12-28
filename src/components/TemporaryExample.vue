db179b2d95a38ff593323f18784d3da264e3692f
chendaozhang <1205308740@qq.com>
2022-12-28 16:01:18 +0800

    完成所有组件文件创建

---
+++ src/components/TemporaryExample.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Example</h1>
+  <h1>Button</h1>
+  <Button />
+  <hr>
+  <h1>Switch</h1>
+  <Switch />
+  <hr>
+  <h1>Dialog</h1>
+  <Dialog />
+  <hr>
+  <h1>Tab</h1>
+  <Tab>
+    <Tabs />
+  </Tab>
+</template>
+
+<script setup lang="ts">
+
+
+import Button from "../lib/Button.vue";
+import Switch from "../lib/Switch.vue";
+import Dialog from "../lib/Dialog.vue";
+import Tab from "../lib/Tab.vue";
+import Tabs from "../lib/Tabs.vue";
+</script>
---
+++ src/lib/Tabs.vue
@@ -0,0 +1,11 @@
+<template>
+<div>Tabs</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+
+</style>
