9d1d75a8aa3271b26c3c567c72d0eed7321f74b5
chendaozhang <1205308740@qq.com>
2022-12-29 16:10:50 +0800

    引入vite-svg-loader

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,8 +1,20 @@
 <template>
-<div>Dialog</div>
+<div>
+  <Teleport to="body">
+    <div></div>
+    <div>
+      <header>
+        <Close />
+      </header>
+      <main></main>
+      <footer></footer>
+    </div>
+  </Teleport>
+</div>
 </template>
 
 <script setup lang="ts">
+import Close from '../assets/close.svg'
 
 </script>
 
