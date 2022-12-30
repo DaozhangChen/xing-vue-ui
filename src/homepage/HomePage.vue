1f06de4670b0a66a366ea0daf1a7ca502fb26c64
chendaozhang <1205308740@qq.com>
2022-12-30 15:29:07 +0800

    完成slogan

--- src/homepage/HomePage.vue
+++ src/homepage/HomePage.vue
@@ -2,12 +2,22 @@
   <ShareHeader />
 <main>
   <video src="../assets/background.mp4" class="backgroundVideo" autoplay="autoplay" loop="loop" muted="muted" />
+  <div class="outerMainText">
+    <div class="mainSlogan">
+      <h1>Xing UI</h1>
+      <p>&#12300; 一款基于Vue的UI组件，简化您的开发流程 &#12301;</p>
+      <RouterLink to="/introduction">
+        <Button theme="primary">开始使用</Button>
+      </RouterLink>
+    </div>
+  </div>
 </main>
 </template>
 
 <script setup lang="ts">
 
 import ShareHeader from "./share/ShareHeader.vue";
+import Button from "../lib/Button.vue";
 </script>
 
 <style lang="scss" scoped>
@@ -16,5 +26,25 @@ import ShareHeader from "./share/ShareHeader.vue";
   width: 100%;
   height: 40vh;
   filter:hue-rotate(270deg);
+}
+.outerMainText{
+  padding: 80px 0;
+  .mainSlogan {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    & h1 {
+      font-size: 64px;
+      font-weight: 1000;
+      margin-bottom: 20px;
+    }
+    & p {
+      font-size: 24px;
+      line-height: 1.5;
+      color: rgba(0, 0, 0, 0.85);
+      word-break: break-word;
+      margin-bottom: 20px;
+    }
+  }
 }
 </style>
