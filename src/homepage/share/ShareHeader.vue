2e416a332cd324ce2c16567a6ee6ad322ffcdd90
chendaozhang <1205308740@qq.com>
2022-12-30 15:15:17 +0800

    初步完成shareHeader

--- src/homepage/share/ShareHeader.vue
+++ src/homepage/share/ShareHeader.vue
@@ -1,28 +1,71 @@
 <template>
-<header>
+<header class="layoutHeader">
   <router-link to="/">
-    <div>
-      <MainLogo class="main-icon"/>
-      XING UI
+    <div class="iconAndTitle">
+      <img src="../../assets/logo.png" class="mainLogo" />
+      <h2> XING UI</h2>
     </div>
   </router-link>
-  <div>
+  <div class="headerRight">
     <ul>
       <li>React版</li>
     </ul>
-    <a target="_blank" href="https://github.com/DaozhangChen/Xing-react-ui"><GitHub /></a>
+    <a target="_blank" href="https://github.com/DaozhangChen/Xing-react-ui"><GitHub class="icon"/></a>
   </div>
 </header>
 </template>
 
 <script setup lang="ts">
-import MainLogo from '../../assets/mainLogo.svg'
 import GitHub from '../../assets/github.svg'
 
 </script>
 
 <style lang="scss" scoped>
-.main-icon{
+.layoutHeader{
+  padding: 0 40px;
+  display: flex;
+  justify-content: space-between;
+  height: 64px;
+  align-items: center;
+  & .iconAndTitle{
+    display: flex;
+    align-items: center;
+    .mainLogo{
+      height: 32px;
+      margin-right: 10px;
+    }
+    h2{
+      font-size: 18px;
+    }
+  }
+  & .headerRight{
+    display: flex;
+    align-items: center;
+    & li{
+      line-height: 58px;
+      cursor: pointer;
+      border-top: 3px solid white;
+      border-bottom:3px solid white;
+      transition: border-top-color 0.25s linear;
+      margin: 0 10px;
+    }
+    li:hover{
+      border-top-color:var(--main-color);
+    }
+    a{
+      width: 32px;
+      height: 32px;
+    }
+    .icon{
+      height: 32px;
+      width: 32px;
+      transition: all 0.25s linear;
+    }
+    .icon:hover{
+      transform: scale(1.05);
+    }
+  }
 }
+
 
 </style>
