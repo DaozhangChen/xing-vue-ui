f52edd4e6d19a2e79c2b56f2357600cf053c4869
chendaozhang <1205308740@qq.com>
2022-12-30 15:20:49 +0800

    引入backgroundVideo

--- src/homepage/HomePage.vue
+++ src/homepage/HomePage.vue
@@ -1,6 +1,8 @@
 <template>
   <ShareHeader />
-<div>homePage</div>
+<main>
+  <video src="../assets/background.mp4" class="backgroundVideo" autoplay="autoplay" loop="loop" muted="muted" />
+</main>
 </template>
 
 <script setup lang="ts">
@@ -8,6 +10,11 @@
 import ShareHeader from "./share/ShareHeader.vue";
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.backgroundVideo{
+  object-fit: fill;
+  width: 100%;
+  height: 40vh;
+  filter:hue-rotate(270deg);
+}
 </style>
--- src/homepage/Introduction.vue
+++ src/homepage/Introduction.vue
@@ -1,9 +1,11 @@
 <template>
+  <ShareHeader />
 <div>introduction</div>
 </template>
 
 <script setup lang="ts">
 
+import ShareHeader from "./share/ShareHeader.vue";
 </script>
 
 <style lang="scss">
