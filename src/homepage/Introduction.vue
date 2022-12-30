4dfc199e4e8ae9237a02c4c50438e0331aeae284
chendaozhang <1205308740@qq.com>
2022-12-30 16:36:41 +0800

    初步完成introduction布局

--- src/homepage/Introduction.vue
+++ src/homepage/Introduction.vue
@@ -1,5 +1,6 @@
 <template>
   <ShareHeader />
+  <div class="mainWrapper">
 <aside class="mainAside">
   <ul>
     <li>如何使用</li>
@@ -13,8 +14,11 @@
       </li>
     </template>
   </ul>
-
 </aside>
+<main class="mainContent">
+    content
+</main>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -23,40 +27,48 @@ import {componentLists} from "./share/componentLists";
 </script>
 
 <style lang="scss" scoped>
-.mainAside{
-  border: 1px solid red;
-  width: 25%;
-  height: calc(100vh - 64px);
-  background: white;
-  & ul:first-child{
-    padding-top: 40px;
-    height: 80px;
-  }
-  & div{
-    font-size: 18px;
-    width: 100%;
-    padding-left:30px;
-    line-height: 40px;
-    font-weight: 600;
-    flex-wrap: nowrap;
-    overflow: hidden;
-  }
-  & li{
-    line-height: 40px;
-    height: 40px;
-    padding-left:30px;
-    border-radius: 6px;
-    overflow: hidden;
-    & .text{
-      color: rgba(0,0,0,0.6);
+.mainWrapper{
+  display: flex;
+  .mainAside{
+    width: 25%;
+    height: calc(100vh - 64px);
+    background: white;
+    & ul:first-child{
+      padding-top: 40px;
+      height: 80px;
     }
-    transition: 0.25s background-color linear;
+    & div{
+      font-size: 18px;
+      width: 100%;
+      padding-left:30px;
+      line-height: 40px;
+      font-weight: 600;
+      flex-wrap: nowrap;
+      overflow: hidden;
+    }
+    & li{
+      line-height: 40px;
+      height: 40px;
+      padding-left:30px;
+      border-radius: 6px;
+      overflow: hidden;
+      & .text{
+        color: rgba(0,0,0,0.6);
+      }
+      transition: 0.25s background-color linear;
+    }
+    & li:hover{
+      background-color:var(--main-color-hover);
+    }
+    & .selected{
+      background-color:rgba(84,66,225,0.5);
+    }
   }
-  & li:hover{
-    background-color:var(--main-color-hover);
-  }
-  & .selected{
-    background-color:rgba(84,66,225,0.5);
+  .mainContent{
+    width: 75%;
+    height: calc(100vh - 64px);
+    overflow: scroll;
+    padding: 40px 32px 32px 64px;
   }
 }
 </style>
